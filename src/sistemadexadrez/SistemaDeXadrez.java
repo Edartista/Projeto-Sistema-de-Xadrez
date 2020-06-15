1bbc2614ae691c55a11a03817895882849870e0a
Edartista <f.vinicius.edart@gmail.com>
2020-06-15 01:34:42 -0300

    Primeira Classe: Posição

--- src/sistemadexadrez/SistemaDeXadrez.java
+++ src/sistemadexadrez/SistemaDeXadrez.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sistemadexadrez;
 
-/**
- *
- * @author Prof de Filosofia
- */
-public class SistemaDeXadrez {
+import Tabuleiro.Posição;
 
-    /**
-     * @param args the command line arguments
-     */
+public class SistemaDeXadrez {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Posição pos = new Posição (3, 5);
+        System.out.println(pos);
     }
     
 }
